98c23010a3639ff10eca020825e0f32595130e3e
asvyryda <asvyryda@student.ucode.world>
2020-12-23 15:57:06 +0200

    main controller

--- src/main/java/world/ucode/cashflow/controllers/MainController.java
+++ src/main/java/world/ucode/cashflow/controllers/MainController.java
@@ -1,14 +1,25 @@
 package world.ucode.cashflow.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import world.ucode.cashflow.models.Wallet;
+import world.ucode.cashflow.repositories.WalletRepo;
 
-@Controller
-@RequestMapping
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
 public class MainController {
+    @Autowired
+    WalletRepo walletRepo;
     @GetMapping
-    public String getMain() {
-        return "main";
+    public List<Wallet> getMain(HttpServletResponse response) throws IOException {
+        response.encodeRedirectURL("main");
+        return walletRepo.findByUser_Id(1);
     }
 }
